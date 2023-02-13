889016cdd06a2e8fc41c6a7b13cee780a0525bbd
iwtkmn0219 <iwtkmn0219@naver.com>
2023-02-13 20:32:39 +0900

    ✨ Add Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+function Nav(props) {
+  const ls = props.menus.map((tmp) => <li key={tmp.id}>{tmp.title}</li>);
+  return (
+    <nav>
+      <ol>{ls}</ol>
+    </nav>
+  );
+}
+
+function Header() {
+  const menus = [
+    { id: 1, title: "menu1" },
+    { id: 2, title: "menu2" },
+    { id: 3, title: "menu3" },
+  ];
+  return (
+    <div>
+      <div>
+        <h1>iwtkmn0219</h1>
+      </div>
+      <Nav menus={menus}></Nav>
+    </div>
+  );
+}
+
 function App() {
-  return <div>iwtkmn0219</div>;
+  return <Header></Header>;
 }
 
 export default App;
